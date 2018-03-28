94342f6441356b15fe2e398547b5dd7730c17376
Cazals <thomas.cazals@viacesi.fr>
2018-03-28 15:37:17 +0200

    Liste Client en cours

--- Madera/Madera/View/Pages/Clients/Index.xaml.cs
+++ Madera/Madera/View/Pages/Clients/Index.xaml.cs
@@ -1,5 +1,7 @@
-﻿using Madera.View.Pages.Tdb;
+﻿using Madera.Model;
+using Madera.View.Pages.Tdb;
 using MahApps.Metro.Controls;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -12,6 +14,9 @@ namespace Madera.View.Pages.Clients
     {
         public Index() {
             InitializeComponent();
+            loadClient();
+           
+            var k = 2;
         }
 
         private void Click_btn_retour(object sender, RoutedEventArgs e) {
@@ -27,6 +32,33 @@ namespace Madera.View.Pages.Clients
         private void btn_edit_client(object sender, RoutedEventArgs e) {
             Edit edit_client = new Edit();
             ((MetroWindow)this.Parent).Content = edit_client;
+        }
+
+        private void loadClient()
+        {
+            DBEntities DB = new DBEntities();
+            
+            ClientList.DataContext = DB.Client.Select(i=>i).ToList().Count();
+
+
+
+            var k= DB.Client.Select(i => i).ToList().Count();
+
+
+
+            // Solution qui marche MVM
+            //for (int i = 0; i < k; i++)
+            //{
+            //    RowDefinition myRow = new RowDefinition();
+            //    ClientList.RowDefinitions.Add(myRow);
+
+            //    Label lab = new Label();
+            //    lab.Content = "fghjkl";
+            //    Grid.SetColumn(lab, 1);
+            //    Grid.SetRow(lab, i);
+            //    ClientList.Children.Add(lab);
+            //}
+            var b = 2;
         }
     }
 }
