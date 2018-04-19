08fc584035862bff2c8a839771b3dd926172a68d
Mathmath31 <le_math@live.fr>
2018-04-19 20:59:35 +0200

    Vue 2D

--- Madera/Madera/View/Pages/Devis/ChoixEmpreinte.xaml.cs
+++ Madera/Madera/View/Pages/Devis/ChoixEmpreinte.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Madera.View.Pages.Tdb;
+﻿using Madera.Model;
+using Madera.View.Pages.PlanVues;
+using Madera.View.Pages.Tdb;
 using MahApps.Metro.Controls;
 using System;
 using System.Collections.Generic;
@@ -25,12 +27,69 @@ namespace Madera.View.Pages.Devis
         public ChoixEmpreinte()
         {
             InitializeComponent();
+            ChargerEmpreinte();
         }
 
         private void Click_btn_retour(object sender, RoutedEventArgs e)
         {
             Tableau_de_bord tdb = new Tableau_de_bord();
             ((MetroWindow)this.Parent).Content = tdb;
+        }
+
+        private void ChargerEmpreinte()
+        {
+            //lsvEmpreinte.View = ListView.ViewProperty.
+            DBEntities DB = new DBEntities();
+            foreach (var item in DB.Empreinte)
+            {
+                //ListViewItem myItem = new ListViewItem();
+                //Image img = new Image();
+                //img.Source = new BitmapImage(new Uri("../../../Pictures/home.png", UriKind.Relative));
+                //myItem.Content = img;
+                //this.lstV.Items.Add(myItem);
+                this.lstV.Items.Add(new Image() { Source = new BitmapImage(new Uri("../../../Pictures/" + item.nom + ".png", UriKind.Relative)) });
+
+            }
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //int test = lstV.SelectedIndex;
+            int col = 0;
+            int lig = 0;
+            //int i = 0;
+            int idZoneMorte = 0;
+
+
+            DBEntities DB = new DBEntities();
+            //foreach (var item in DB.Empreinte)
+            //{
+            //    if (i == test)
+            //    {
+            //        col = (int)item.longueur;
+            //        lig = (int)item.largeur;
+            //        idZoneMorte = (int)item.idZoneMorte;
+            //        break;
+            //    }
+            //    i++;
+            //}
+
+            int bb = lstV.SelectedIndex + 1;
+            Empreinte empreinteSelection = new Empreinte();
+            empreinteSelection = DB.Empreinte.Where(i => i.idEmpreinte == bb).FirstOrDefault();
+
+            col = (int)empreinteSelection.longueur;
+            lig = (int)empreinteSelection.largeur;
+            if (empreinteSelection.idZoneMorte != null)
+            {
+                idZoneMorte = (int)empreinteSelection.idZoneMorte;
+            }
+
+
+            Vue2D vue2D = new Vue2D(col, lig, idZoneMorte);
+            ((MetroWindow)this.Parent).Content = vue2D;
         }
     }
 }
--- Madera/Madera/View/Pages/PlanVues/Vue2D.xaml.cs
+++ Madera/Madera/View/Pages/PlanVues/Vue2D.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Madera.Model;
+using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,259 @@ namespace Madera.View.Pages.PlanVues
     /// </summary>
     public partial class Vue2D : Page
     {
-        public Vue2D()
+        public Vue2D(int col, int lig, int idZoneMorte)
         {
             InitializeComponent();
+            //test maj github
+            CreateEmptyFloorPlan(col, lig, idZoneMorte);
+        }
+
+        private void CreateEmptyFloorPlan(int col, int lig, int idZoneMorte)
+        {
+            col = (col * 2 + 1);
+            lig = (lig * 2 + 1);
+
+            int zoneMorteTailleX = 0;
+            int zoneMorteTailleY = 0;
+            int zoneMorteCoordX = 0;
+            int zoneMorteCoordY = 0;
+
+            DBEntities DB = new DBEntities();
+            foreach (var item in DB.ZoneMorte)
+            {
+                if (idZoneMorte == item.idZoneMorte)
+                {
+                    if ((int)item.coordonneeX != 0)
+                    {
+                        zoneMorteCoordX = ((int)item.coordonneeX * 2 + 1);
+                        zoneMorteTailleX = ((int)item.longueur * 2 + 1);
+                    }
+                    else
+                    {
+                        zoneMorteTailleX = ((int)item.longueur * 2 + 1) + 1;
+                    }
+                    if ((int)item.coordonneeY != 0)
+                    {
+                        zoneMorteCoordY = ((int)item.coordonneeY * 2 + 1);
+                        zoneMorteTailleY = ((int)item.largeur * 2 + 1);
+                    }
+                    else
+                    {
+                        zoneMorteTailleY = ((int)item.largeur * 2 + 1) + 1;
+                    }
+
+                }
+            }
+
+            // Crée le Grid vide
+            // Fois 2 + 1 pour mur (ext et interieur)
+            for (int i = 0; i < lig; i++)
+            {
+                RowDefinition myRow = new RowDefinition();
+                myRow.Height = new GridLength(1, GridUnitType.Star);
+                grid2D.RowDefinitions.Add(myRow);
+            }
+
+            for (int i = 0; i < col; i++)
+            {
+                ColumnDefinition myCol = new ColumnDefinition();
+                myCol.Width = new GridLength(1, GridUnitType.Star);
+                grid2D.ColumnDefinitions.Add(myCol);
+            }
+
+            //test remplissage de la grille avec des bouttons
+            int count = 1;
+            for (int y = 0; y < grid2D.RowDefinitions.Count; y++)
+            {
+                for (int x = 0; x < grid2D.ColumnDefinitions.Count; x++)
+                {
+                    //ne tracer que les murs
+                    if ((((y % 2) == 0) && ((x % 2) != 0)) || (((y % 2) != 0) && ((x % 2) == 0)))
+                    {
+                        //ne pas tracer la zone morte
+                        if ((col - zoneMorteCoordX - zoneMorteTailleX + 1 != 0 && x == zoneMorteCoordX + zoneMorteTailleX - 2) ||
+                            (lig - zoneMorteCoordY - zoneMorteTailleY + 1 != 0 && y == zoneMorteCoordY + zoneMorteTailleY - 2) ||
+                            y < zoneMorteCoordY ||
+                            x < zoneMorteCoordX ||
+                            y > zoneMorteCoordY + zoneMorteTailleY - 2 ||
+                            x > zoneMorteCoordX + zoneMorteTailleX - 2)
+                        {
+
+
+                            //TODO faire des templates de button
+                            Button MyControl1 = new Button();
+                            MyControl1.Content = ("x" + (x + 1).ToString() + " y" + (y + 1).ToString());
+                            MyControl1.Name = "Button" + count.ToString();
+                            Grid.SetColumn(MyControl1, x);
+                            Grid.SetRow(MyControl1, y);
+
+                            //TODO chercher les murs ext
+                            if ((y == 0 && (x < zoneMorteCoordX || x > zoneMorteCoordX + zoneMorteTailleX - 2)) || (x == 0 && (y < zoneMorteCoordY || y > zoneMorteCoordY + zoneMorteTailleY - 2)))
+                            {
+                                MyControl1.Background = btnMurExt.Background;
+                                MyControl1.Click += new RoutedEventHandler(btnClickMurExt);
+                            }
+                            else
+                            {
+                                MyControl1.Click += new RoutedEventHandler(btnClick);
+                            }
+
+                            //if ((y == lig - 1 && (x < zoneMorteCoordX || x > zoneMorteCoordX + zoneMorteTailleX - 2)) || (x == col - 1 && (y < zoneMorteCoordY || y > zoneMorteCoordY + zoneMorteTailleY - 2)))
+                            //{
+                            //    MyControl1.Background = btnMurExt.Background;
+                            //}
+
+                            //MyControl1.Click += new RoutedEventHandler(btnClick);
+                            //MyControl1.PreviewMouseDown += new MouseButtonEventHandler(btnDown);
+                            //MyControl1.PreviewMouseUp += new MouseButtonEventHandler(btnUp);
+                            //MyControl1.MouseEnter += new System.Windows.Input.MouseEventArgs(mouseEnter);
+                            grid2D.Children.Add(MyControl1);
+
+                            count++;
+
+                        }
+                        else
+                        {
+
+                        }
+
+                    }
+                }
+            }
+
+            //Taille des boutons
+            for (int i = 0; i < grid2D.ColumnDefinitions.Count; i++)
+            {
+                if ((i % 2) == 0)
+                    grid2D.ColumnDefinitions[i].Width = new GridLength(15);
+            }
+
+            for (int i = 0; i < grid2D.RowDefinitions.Count; i++)
+            {
+                if ((i % 2) == 0)
+                    grid2D.RowDefinitions[i].Height = new GridLength(15);
+
+            }
+
+            //Couleur des murs Exterieurs
+            //foreach (Control ctrl in grid2D.Children)
+            //{
+            //    if (ctrl.GetType() == typeof(Button))
+            //    {
+
+            //        Button btn = (Button)ctrl;
+
+            //        if (Grid.GetRow(ctrl) == 0)
+            //        {
+            //            btn.Background = btnMurExt.Background;
+            //        }
+
+            //        if (Grid.GetColumn(ctrl) == 0)
+            //        {
+            //            btn.Background = btnMurExt.Background;
+            //        }
+
+            //        if (Grid.GetRow(ctrl) == (lig - 1))
+            //        {
+            //            if (Grid.GetRow(ctrl) != zoneMorteCoordX * 2)
+            //            {
+            //                btn.Background = btnMurExt.Background;
+            //            }
+
+            //        }
+
+            //        if (Grid.GetColumn(ctrl) == (col - 1))
+            //        {
+            //            btn.Background = btnMurExt.Background;
+            //        }
+            //    }
+            //}
+
+            //for (int y = 0; y < grid2D.RowDefinitions.Count; y = y + 2)
+            //{
+            //    var test = grid2D.RowDefinitions[y + 2];
+
+            //    //MessageBox.Show(grid2D.RowDefinitions[y + 2].GetChildObjects);
+            //    if (grid2D.RowDefinitions[y + 2] != null)
+            //    {
+
+            //    }
+            //}
+
+            //for (int x = 0; x < grid2D.ColumnDefinitions.Count; x = x + 2)
+            //{
+
+            //}
+
+
+
+        }
+
+        private void btnClickMurExt(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClick(object sender, RoutedEventArgs e)
+        {
+            //Button obj = ((FrameworkElement)sender).DataContext as Button;
+
+            //MessageBox.Show(sender.ToString());
+            if (rbMurExt.IsChecked == true)
+            {
+                ((Button)sender).Background = btnMurExt.Background;
+            }
+            else if (rbMurInt.IsChecked == true)
+            {
+                ((Button)sender).Background = btnMurInt.Background;
+            }
+            else if (rbPorte.IsChecked == true)
+            {
+                ((Button)sender).Background = btnPorte.Background;
+            }
+            else if (rbFenetre.IsChecked == true)
+            {
+                ((Button)sender).Background = btnFenetre.Background;
+            }
+            else if (rbLibre.IsChecked == true)
+            {
+                ((Button)sender).Background = btnLibre.Background;
+            }
+
+        }
+
+        private void mouseEnter(object sender, MouseEventArgs e)
+        {
+            //if ((Mouse)e.Button == MouseButtons.Left)
+            //{
+
+            //}
+        }
+
+        private void btnUp(object sender, MouseButtonEventArgs e)
+        {
+
+            int row = Grid.GetRow(sender as Button);
+            int column = Grid.GetColumn(sender as Button);
+            MessageBox.Show("row " + row + " column " + column);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDown(object sender, MouseButtonEventArgs e)
+        {
+            //enregistre position en x et y du bouton
+            //int row = Grid.GetRow(sender as Button);
+            //int column = Grid.GetColumn(sender as Button);
+            //MessageBox.Show("row " + row + " column " + column);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Apercu3D windows3D = new Apercu3D();
+            // ((MetroWindow)this.Parent).Content = windows3D;
         }
     }
 }
