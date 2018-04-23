58e372228cffae9286affa3ebddcb79c7b1fb4e5
Cazals <thomas.cazals@viacesi.fr>
2018-04-23 18:57:26 +0200

    Liste 2D manque Container global et bouton Couleur

--- Madera/Madera/View/Pages/PlanVues/Vue2D.xaml.cs
+++ Madera/Madera/View/Pages/PlanVues/Vue2D.xaml.cs
@@ -37,31 +37,33 @@ namespace Madera.View.Pages.PlanVues
             listGamme.SelectedValuePath = "idGamme";
             listGamme.SelectedIndex = 0; // Selection premier champ de la liste
 
-            //
-
+            //Type Module, liste avec critère de gamme
             List<TypeModule> TypeModuleList = new List<TypeModule>();
             TypeModuleList = DB.TypeModule.ToList();
             listTypeModule.ItemsSource = TypeModuleList;
+            listTypeModule.SelectedValuePath = "idType";
             listTypeModule.SelectedIndex = 0;
 
+            //Finition, liste avec critère de gamme
+            List<Finition> FinitionList = new List<Finition>();
+            FinitionList = DB.Finition.ToList();
+            listFinition.ItemsSource = FinitionList;
+            listFinition.SelectedValuePath = "idFinition";
+            listFinition.SelectedIndex = 0;
 
+            //Liste des modules ayant la gammme, le type module et la finition selectionnée
             List<Module> ModuleList = new List<Module>();
-            ModuleList = DB.Module.Where(i=>i.TypeModule.idType == listTypeModule.SelectedIndex+1).ToList();
+            ModuleList = DB.Module.Where(i=>i.TypeModule.idType == (long)listTypeModule.SelectedValue
+            && i.Gamme.idGamme == (long)listGamme.SelectedValue).ToList();
             listModule.ItemsSource = ModuleList;
-
+            listFinition.SelectedValuePath = "idModule";
 
             // Couleur liste indépendante
             List<Couleur> CouleurList = new List<Couleur>();
             CouleurList = DB.Couleur.ToList();
             listCouleur.ItemsSource = CouleurList;
+            listCouleur.SelectedValuePath = "idCouleur";
 
-
-            
-
-
-            List<Finition> FinitionList = new List<Finition>();
-            FinitionList = DB.Finition.ToList();
-            listFinition.ItemsSource = FinitionList;
         }
 
 
