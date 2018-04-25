531dd255ad6dafc109a4dc06c6309902c4d0d55f
Tom BRAZZALOTTO <tom.brazzalotto@viacesi.fr>
2018-04-25 19:40:27 +0200

    Récupération des modules et listing de la facture

--- Madera/Madera/View/Pages/Factures/ModelPDF.xaml.cs
+++ Madera/Madera/View/Pages/Factures/ModelPDF.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using iTextSharp.text;
 using iTextSharp.text.pdf;
+using Madera.Model;
 using Madera.View.Pages.Tdb;
 using MahApps.Metro.Controls;
+using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -16,19 +19,58 @@ namespace Madera.View.Pages.Factures
     /// </summary>
     public partial class ModelPDF : Page
     {
-        public ModelPDF() {
+        public ModelPDF(int id_maison) {
             InitializeComponent();
+            GetInfosClient(id_maison);
+            getAllModuleMaison(id_maison);
         }
 
         private void Click_btn_retour(object sender, RoutedEventArgs e) {
-            Tableau_de_bord tdb = new Tableau_de_bord();
-            ((MetroWindow)this.Parent).Content = tdb;
+            Factures.Index listing_facture = new Factures.Index();
+            ((MetroWindow)this.Parent).Content = listing_facture;
         }
 
-#region pdf
-        private void btn_print_Click(object sender, RoutedEventArgs e) {
-            var client = "BRAZZALOTTO CAZES";
-            string sPDFFileName = System.IO.Path.GetTempPath() + "Facture_"+ client + ".pdf";
+        // Récupère les infos du client
+        private void GetInfosClient(int id_maison) {
+            DBEntities db = new DBEntities();
+
+            Projet ProjMaison = new Projet();
+            Client clientMaison = new Client();
+            ProjMaison = db.Projet.Where(i => i.idMaison == id_maison).FirstOrDefault();
+            clientMaison = db.Client.Where(i => i.idClient == ProjMaison.idClient).FirstOrDefault();
+
+            nom_client.Content = clientMaison.nom + " " + clientMaison.prenom;
+            adresse_client.Text = clientMaison.adresse;
+        }
+
+        private void getAllModuleMaison(int id_maison) {
+            DBEntities db = new DBEntities();
+
+            var liste_module = from module_maison in db.Module_Maison
+                               join module in db.Module on module_maison.idModule equals module.idModule
+                               join type_module in db.TypeModule on module.idType equals type_module.idType
+                               join gamme in db.Gamme on module.idGamme equals gamme.idGamme
+                               where module_maison.idMaison == id_maison
+                               select new {
+                                   nom_module = module.nom,
+                                   prix_module = module.prix,
+                                   image_module = module.imgUrl,
+                                   type_module = type_module.nomType,
+                                   nom_gamme = gamme.nom,
+                               };
+
+            listing_modules.ItemsSource = liste_module.ToList();
+            var prix = 0;
+            foreach(var module in liste_module) {
+                prix += (int)module.prix_module;
+            }
+            prix_total.Content = "Prix total :"+ prix +" €";
+        }
+
+        #region pdf
+        private void btn_print_Click(object sender, RoutedEventArgs e) {            
+            var date = DateTime.Now.ToString("hh mm ss tt");
+            string sPDFFileName = System.IO.Path.GetTempPath() + "Facture_" + date + ".pdf";
             string sImagePath = System.IO.Path.GetTempPath() + "window.png";
 
             SaveAsPng(GetImage(vue_pdf_facture), sImagePath);
@@ -79,7 +121,7 @@ namespace Madera.View.Pages.Factures
             }
         }
 
-#endregion
+        #endregion
 
     }
 }
